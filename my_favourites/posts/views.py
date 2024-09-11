a884605f624a619a9b0217b91f941d9fc8710030
Adam Ługowski <adam.lugowski@live.com>
2024-09-11 12:41:31 +0200

    added base.html and index.html. styling with bootstrap

--- my_favourites/posts/views.py
+++ my_favourites/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return HttpResponse('Test')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/index.html', context=context)
