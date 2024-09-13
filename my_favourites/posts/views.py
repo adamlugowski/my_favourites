c385c19aefb59131813acefd3f4601c6465dc88f
Adam Ługowski <adam.lugowski@live.com>
2024-09-13 12:09:34 +0200

    added view to create posts and buttons to improve usability

--- my_favourites/posts/views.py
+++ my_favourites/posts/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'posts/index.html', context=context)
+
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:my_posts')
+    else:
+        form = PostForm()
+
+    return render(request, 'posts/add_post.html', {'form': form})
+
+
+@login_required
+def my_posts(request):
+    posts = Post.objects.filter(author=request.user)
+    return render(request, 'posts/my_posts.html', {'posts': posts})
